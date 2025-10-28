b174bb5173bfb46e425a9ee05d801cd578cc4ebf
Tabitha <naylahanegan@gmail.com>
2025-10-28 19:18:42 -0400

    Update marioParty7.py

--- codes/marioParty7.py
+++ codes/marioParty7.py
@@ -8,14 +8,18 @@
 def getBlueSpaceCodeSeven(amount, amountDec):
     return f'''
 MP7 - Blue Spaces Give {amountDec} Coins
-C2168578 00000001
+C2168577 00000001
+60000000 00000000
+C216857C 00000001
 3880{amount} 00000000
 '''
 
 def getRedSpaceCodeSeven(amount, amountDec):
     return f'''
 MP7 - Red Spaces Take Away {amountDec} Coins
-C2168600 00000001
+04168600 00000001
+60000000 00000000
+C2168604 00000001
 3880{amount} 00000000
 '''
 
